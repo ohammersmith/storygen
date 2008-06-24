83d2bb0ef75ffa1bcd5c6f747ce7e608facabf78
Otto Hammersmith <otto.hammersmith@gmail.com>
2008-06-24 19:21:26 -0400

    Changing the name of the plain text story and ruby runner file.

--- generators/rspec_story/rspec_story_generator.rb
+++ generators/rspec_story/rspec_story_generator.rb
@@ -13,10 +13,10 @@ class RspecStoryGenerator < Rails::Generator::NamedBase
       m.directory File.join("stories", "steps")
 
       # Plain text story template
-      m.template 'plain_text_story.txt', File.join('stories', "#{story_name}")
+      m.template 'plain_text_story.txt', File.join('stories', "#{story_name}_story")
 
       # Main story file
-      m.template 'story.rb', File.join('stories', "#{story_name}.rb")
+      m.template 'story.rb', File.join('stories', "#{story_name}_story.rb")
 
       # Story steps file
       m.template 'story_steps.rb', File.join('stories', "steps", "#{story_name}_steps.rb")
