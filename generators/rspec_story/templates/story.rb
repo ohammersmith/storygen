1639a62ab9c071084d77c378a8e009221850c996
Otto Hammersmith <otto.hammersmith@gmail.com>
2008-06-25 13:26:57 -0400

    Forgot the RailsStory option on the run line.

--- generators/rspec_story/templates/story.rb
+++ generators/rspec_story/templates/story.rb
@@ -1,5 +1,5 @@
 require File.dirname(__FILE__) + "/helper"
 
 with_steps_for :<%= story_name %> do
-	run File.expand_path(File.dirname(__FILE__) + "/stories/" + File.basename(__FILE__).gsub(".rb",".story"))
+	run File.expand_path(File.dirname(__FILE__) + "/stories/" + File.basename(__FILE__).gsub(".rb",".story")), :type => RailsStory
 end
